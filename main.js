f2431417ea7e94d13fda493427dd28b9bfeb70c4
Nicolas Harvey Nye <nyenicolas@gmail.com>
2026-04-10 12:17:25 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,51 @@
+const tablasSnowboard = [
+    {
+    marca: 'Jones',
+    modelo: 'Stratos',
+    altura: 158 ,
+    especialidad: 'Freeride',
+    shape: 'Direccional',
+    perfil: 'camber'
+},
+    {
+    marca: 'Burton',
+    modelo: 'Camber',
+    altura: 155,
+    especialidad: 'All-mountain',
+    shape: 'Direccional',
+    perfil: 'camber'
+},
+    {
+    marca: 'Burton',
+    modelo: 'Custom X',
+    altura: 158,
+    especialidad: 'Pista',
+    shape: 'Direccional',
+    perfil: 'Camber'
+},
+    {
+    marca: 'Burton',
+    modelo: 'Instigator',
+    altura: 150,
+    especialidad: 'Pista / Snowpark',
+    shape: 'Twin-tip',
+    perfil: 'Flat-Top' ,
+},
+    {
+    marca: 'Nitro',
+    modelo: 'Team',
+    altura: 149,
+    especialidad: 'Snowpark',
+    shape:'Twin-tip',
+    perfil: 'Flying V'
+},
+    {
+    marca: 'Vio Snowboards',
+    modelo: 'The Pineral',
+    altura: 145,
+    especialidad: 'All-mountain',
+    shape: 'Twin-tip',
+    perfil: 'Camber'
+}    
+]
+
