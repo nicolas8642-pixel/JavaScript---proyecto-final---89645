037c9b8209ab60d8be4314807d7bbec0bc769a92
Nicolas Harvey Nye <nyenicolas@gmail.com>
2026-04-16 17:42:14 -0300

    frente comletado

--- main.js
+++ main.js
@@ -5,15 +5,17 @@ const tablasSnowboard = [
     altura: 158 ,
     especialidad: 'Freeride',
     shape: 'Direccional',
-    perfil: 'camber'
+    perfil: 'camber',
+    id: 1
 },
     {
     marca: 'Burton',
-    modelo: 'Camber',
+    modelo: 'Custom',
     altura: 155,
     especialidad: 'All-mountain',
     shape: 'Direccional',
-    perfil: 'camber'
+    perfil: 'camber',
+    id: 2
 },
     {
     marca: 'Burton',
@@ -21,7 +23,8 @@ const tablasSnowboard = [
     altura: 158,
     especialidad: 'Pista',
     shape: 'Direccional',
-    perfil: 'Camber'
+    perfil: 'Camber',
+    id: 3
 },
     {
     marca: 'Burton',
@@ -29,7 +32,8 @@ const tablasSnowboard = [
     altura: 150,
     especialidad: 'Pista / Snowpark',
     shape: 'Twin-tip',
-    perfil: 'Flat-Top' ,
+    perfil: 'Flat-Top',
+    id: 4
 },
     {
     marca: 'Nitro',
@@ -37,7 +41,8 @@ const tablasSnowboard = [
     altura: 149,
     especialidad: 'Snowpark',
     shape:'Twin-tip',
-    perfil: 'Flying V'
+    perfil: 'Flying V',
+    id: 5
 },
     {
     marca: 'Vio Snowboards',
@@ -45,8 +50,37 @@ const tablasSnowboard = [
     altura: 145,
     especialidad: 'All-mountain',
     shape: 'Twin-tip',
-    perfil: 'Camber'
-}    
+    perfil: 'Camber',
+    id: 6
+},
+,{
+    marca: 'Nitro', 
+    modelo: 'Ripper', 
+    altura: 145,
+    especialidad: 'All-mountain',
+    shape: 'Twin-tip',
+    perfil: 'Camber',
+    id: 7
+}
+,{
+    marca: 'Capita', 
+    modelo: 'Dark Horse', 
+    altura: 156,
+    especialidad: 'Snowpark',
+    shape: 'Direccional',
+    perfil: 'Camber',
+    id: 8
+}
+,{
+    marca: 'K2', 
+    modelo: 'Hypnotist', 
+    altura: 160,
+    especialidad: 'All-mountain',
+    shape: 'Twin-direccional',
+    perfil: 'Camber',
+    id: 9
+}
+
 ]
 
 console.log(tablasSnowboard);
